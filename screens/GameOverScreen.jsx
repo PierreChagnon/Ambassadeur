74c804b09524e74987bc0f1b348baefe5c13fd50
PierreChagnon <pc.chagnon@gmail.com>
2022-07-11 19:12:03 +0200

    gameover screen autoscroll

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Image, ScrollView } from "react-native";
+import React, { useContext, useEffect, useState, useRef } from "react";
+import { View, Image, ScrollView, Animated } from "react-native";
 
 import BottomButton from "../components/BottomButton"
 import CustomText from "../components/CustomText";
@@ -10,6 +10,17 @@ import styles from "../styles";
 
 export default function ({ navigation }) {
     const state = useContext(StateContext)
+    const scrollAnim = useRef(new Animated.Value(0)).current
+
+    const listHeight = 40 * state.wordsList.length
+
+    // useEffect(() => {
+    //     Animated.timing(scrollAnim, {
+    //         toValue: -200,
+    //         duration: 10000,
+    //         useNativeDriver: false
+    //     }).start();
+    // }, [])
 
     const handleReplay = () => {
         navigation.navigate("Teams");
@@ -27,6 +38,11 @@ export default function ({ navigation }) {
 
     }
 
+    const onScrollHandler = () => {
+        //reset la valeur de "top" pour les items de la scrollview
+        scrollAnim.setValue(0)
+    }
+
     return (
         <View style={[styles.gameover_container, state.winner === "jaune" ? { backgroundColor: "#F2BB08" } : {}]}>
             <View style={styles.gameover_top_container}>
@@ -40,10 +56,23 @@ export default function ({ navigation }) {
                 <CustomText >
                     L'ÉQUIPE {state.winner.toUpperCase()} A GAGNÉ !
                 </CustomText>
-                <View style={{ borderColor:"#EDFFEC", borderBottomWidth: 2, borderTopWidth: 2, height: "60%" }}>
-                    <ScrollView contentContainerStyle={{padding: 20}} >
+                <View
+                    onLayout={(e) => {
+                        const { x, y, width, height } = e.nativeEvent.layout;
+                        Animated.timing(scrollAnim, {
+                            toValue: -(listHeight - height),
+                            duration: 10000,
+                            useNativeDriver: false
+                        }).start();
+                    }}
+                    style={{ borderColor: "#EDFFEC", borderBottomWidth: 2, borderTopWidth: 2, height: "60%", width: "80%" }}>
+                    <ScrollView scrollEventThrottle={16} onScroll={onScrollHandler} style={{}} contentContainerStyle={{ padding: 20 }} >
                         {state.wordsList.reverse().map((word, i) => {
-                            return <View><CustomText key={i}>{word}</CustomText></View> 
+                            return (
+                                <Animated.View key={i} style={{ marginBottom: 20, top: scrollAnim }}>
+                                    <CustomText >{word}</CustomText>
+                                </Animated.View>
+                            )
                         })}
                     </ScrollView>
                 </View>
